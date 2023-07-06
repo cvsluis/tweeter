5b96b1562e379d224c9524bc834cacc3f12fb1f8
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-07-06 09:35:07 -0700

    feat: implement character counter
    Add jquery function for input event to update character counter
    Implement color changing feature using css

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  // use jquery to get dom object with id of tweet-text
+  // set input event handler and callback function
+  $('#tweet-text').on('input', function() {
+
+    // set characters availalbe to 140 minus length of text area input
+    const charsAvailable = 140 - $(this).val().length;
+    // set variable to dom object with class of counter
+    const counter = $(this).parent().find(".counter");
+
+    // update jquery counter text to be the current value of chars available
+    $(counter).text(charsAvailable);
+
+    // if characters available is less than 0, apply counter-red class
+    if (charsAvailable < 0) {
+      $(counter).addClass("counter-red");
+      // if text limit is more than 0, remove counter-red class
+    } else {
+      $(counter).removeClass("counter-red");
+    }
+
+  });
+});
